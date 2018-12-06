eed5fb61ce65fc7d16403b9910b37abab9e66fd7
Esdras Tarsis <esdrastarsis@gmail.com>
2018-12-06 18:15:46 -0300

    static path fixed

--- gog/games/HREF.py
+++ gog/games/HREF.py
@@ -1,5 +1,5 @@
 import glob
 
 for a in glob.glob("*.html"):
-    print("<li><a href=\"\\games\\{}\">{}</a></li>".format(a, a.split(".")[0]))
+    print("<li><a href=\"\\gog\games\\{}\">{}</a></li>".format(a, a.split(".")[0]))
 output.txt
